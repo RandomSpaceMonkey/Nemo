30e3cb7bbb544b0826e4478c8da9f181ece6f784
Neev Cohen <neevc.dev@gmail.com>
2023-03-12 22:52:24 +0200

    use chunk class for end chunk

--- NemoCpp/NemoLib/include/pngencoder.hh
+++ NemoCpp/NemoLib/include/pngencoder.hh
@@ -6,6 +6,8 @@
 
 #define PNG_HEADER_SIZE 8
 #define IEND_CHUNK_SIZE 12
+#define IEND_CHUNK_LENGTH 0
+#define IEND_CHUNK_TYPE "IEND"
 #define PNG_CHUNK_TYPE_LENGTH 4
 #define PNG_CHUNK_CRC_LENGTH 4
 #define PNG_FILE_EXT "png"
@@ -14,11 +16,11 @@ class PNGEncoder
 {
 public:
     static void encode(std::string filepath, std::string message);
-    static void decode(std::string filepath, std::string message);
+    static std::string decode(std::string filepath);
     static void validate(std::string filepath);
 
 private:
     static const char header[];
-    static const char end_chunk[];
+    static PNGChunk end_chunk;
     static const char nemo_chunk_type[];
 };
--- NemoCpp/NemoLib/src/pngencoder.cc
+++ NemoCpp/NemoLib/src/pngencoder.cc
@@ -5,7 +5,7 @@
 #include "pngchunk.hh"
 
 const char PNGEncoder::header[] = "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a";
-const char PNGEncoder::end_chunk[] = "\x00\x00\x00\x00\x49\x45\x4E\x44\xAE\x42\x60\x82";
+PNGChunk PNGEncoder::end_chunk(IEND_CHUNK_TYPE, nullptr, IEND_CHUNK_LENGTH);
 const char PNGEncoder::nemo_chunk_type[] = "\x6E\x65\x4D\x6F"; // neMo
 
 void PNGEncoder::encode(std::string filepath, std::string message)
@@ -15,9 +15,9 @@ void PNGEncoder::encode(std::string filepath, std::string message)
         std::ofstream ofile;
         ofile.open(filepath, std::ios::binary | std::ios::in | std::ios::ate);
         ofile.seekp(0 - IEND_CHUNK_SIZE, std::ios::cur);
-        PNGChunk chunk(nemo_chunk_type, message.c_str(), message.length());
-        ofile << chunk;
-        ofile.write((char *)PNGEncoder::end_chunk, IEND_CHUNK_SIZE);
+        PNGChunk nemo_chunk(nemo_chunk_type, message.c_str(), message.length());
+        ofile << nemo_chunk;
+        ofile << PNGEncoder::end_chunk;
         ofile.close();
     }
     catch (std::runtime_error)
@@ -26,7 +26,7 @@ void PNGEncoder::encode(std::string filepath, std::string message)
     }
 }
 
-void PNGEncoder::decode(std::string filepath, std::string message)
+std::string PNGEncoder::decode(std::string filepath)
 {
 }
 
